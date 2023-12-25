92f6d65efbedefbf5618bc59825250eae10f36aa
Anton Kraev <antonkraeww@gmail.com>
2023-12-25 06:32:44 +0300

    added TT_API_URL verification for HttpUrl compliance

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+from pydantic import HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    TT_API_URL: str
+    TT_API_URL: HttpUrl
     model_config = SettingsConfigDict(env_file=".env")
